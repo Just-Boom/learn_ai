7a56c948dc2816e3ba84a69a0190174f90a12a4c
Just-Boom <569141331@qq.com>
2022-09-11 23:55:40 +0800

    Add files via upload
    This file is for developing the Car control platform

---
+++ Car_Platform/car_test_form/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,233 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.B_run = New System.Windows.Forms.Button()
+        Me.B_left = New System.Windows.Forms.Button()
+        Me.B_right = New System.Windows.Forms.Button()
+        Me.B_stop = New System.Windows.Forms.Button()
+        Me.B_back = New System.Windows.Forms.Button()
+        Me.B_open = New System.Windows.Forms.Button()
+        Me.B_close = New System.Windows.Forms.Button()
+        Me.B_blue = New System.Windows.Forms.Button()
+        Me.L_state = New System.Windows.Forms.Label()
+        Me.L_speed = New System.Windows.Forms.Label()
+        Me.Text_state = New System.Windows.Forms.TextBox()
+        Me.Text_speed = New System.Windows.Forms.TextBox()
+        Me.Cmb_scan = New System.Windows.Forms.ComboBox()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Cmb_baud = New System.Windows.Forms.ComboBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'B_run
+        '
+        Me.B_run.Location = New System.Drawing.Point(757, 300)
+        Me.B_run.Margin = New System.Windows.Forms.Padding(4)
+        Me.B_run.Name = "B_run"
+        Me.B_run.Size = New System.Drawing.Size(100, 29)
+        Me.B_run.TabIndex = 0
+        Me.B_run.Text = "前进"
+        Me.B_run.UseVisualStyleBackColor = True
+        '
+        'B_left
+        '
+        Me.B_left.Location = New System.Drawing.Point(623, 369)
+        Me.B_left.Margin = New System.Windows.Forms.Padding(4)
+        Me.B_left.Name = "B_left"
+        Me.B_left.Size = New System.Drawing.Size(100, 29)
+        Me.B_left.TabIndex = 1
+        Me.B_left.Text = "左转"
+        Me.B_left.UseVisualStyleBackColor = True
+        '
+        'B_right
+        '
+        Me.B_right.Location = New System.Drawing.Point(895, 369)
+        Me.B_right.Margin = New System.Windows.Forms.Padding(4)
+        Me.B_right.Name = "B_right"
+        Me.B_right.Size = New System.Drawing.Size(100, 29)
+        Me.B_right.TabIndex = 2
+        Me.B_right.Text = "右转"
+        Me.B_right.UseVisualStyleBackColor = True
+        '
+        'B_stop
+        '
+        Me.B_stop.Location = New System.Drawing.Point(757, 369)
+        Me.B_stop.Margin = New System.Windows.Forms.Padding(4)
+        Me.B_stop.Name = "B_stop"
+        Me.B_stop.Size = New System.Drawing.Size(100, 29)
+        Me.B_stop.TabIndex = 3
+        Me.B_stop.Text = "停止"
+        Me.B_stop.UseVisualStyleBackColor = True
+        '
+        'B_back
+        '
+        Me.B_back.Location = New System.Drawing.Point(757, 436)
+        Me.B_back.Margin = New System.Windows.Forms.Padding(4)
+        Me.B_back.Name = "B_back"
+        Me.B_back.Size = New System.Drawing.Size(100, 29)
+        Me.B_back.TabIndex = 4
+        Me.B_back.Text = "后退"
+        Me.B_back.UseVisualStyleBackColor = True
+        '
+        'B_open
+        '
+        Me.B_open.Location = New System.Drawing.Point(13, 72)
+        Me.B_open.Margin = New System.Windows.Forms.Padding(4)
+        Me.B_open.Name = "B_open"
+        Me.B_open.Size = New System.Drawing.Size(100, 29)
+        Me.B_open.TabIndex = 5
+        Me.B_open.Text = "打开"
+        Me.B_open.UseVisualStyleBackColor = True
+        '
+        'B_close
+        '
+        Me.B_close.Location = New System.Drawing.Point(13, 73)
+        Me.B_close.Margin = New System.Windows.Forms.Padding(4)
+        Me.B_close.Name = "B_close"
+        Me.B_close.Size = New System.Drawing.Size(100, 29)
+        Me.B_close.TabIndex = 6
+        Me.B_close.Text = "关闭"
+        Me.B_close.UseVisualStyleBackColor = True
+        '
+        'B_blue
+        '
+        Me.B_blue.Location = New System.Drawing.Point(13, 35)
+        Me.B_blue.Margin = New System.Windows.Forms.Padding(4)
+        Me.B_blue.Name = "B_blue"
+        Me.B_blue.Size = New System.Drawing.Size(100, 29)
+        Me.B_blue.TabIndex = 7
+        Me.B_blue.Text = "蓝牙连接"
+        Me.B_blue.UseVisualStyleBackColor = True
+        '
+        'L_state
+        '
+        Me.L_state.AutoSize = True
+        Me.L_state.Location = New System.Drawing.Point(587, 80)
+        Me.L_state.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.L_state.Name = "L_state"
+        Me.L_state.Size = New System.Drawing.Size(37, 15)
+        Me.L_state.TabIndex = 8
+        Me.L_state.Text = "状态"
+        '
+        'L_speed
+        '
+        Me.L_speed.AutoSize = True
+        Me.L_speed.Location = New System.Drawing.Point(777, 80)
+        Me.L_speed.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.L_speed.Name = "L_speed"
+        Me.L_speed.Size = New System.Drawing.Size(37, 15)
+        Me.L_speed.TabIndex = 9
+        Me.L_speed.Text = "速度"
+        '
+        'Text_state
+        '
+        Me.Text_state.Location = New System.Drawing.Point(589, 111)
+        Me.Text_state.Margin = New System.Windows.Forms.Padding(4)
+        Me.Text_state.Name = "Text_state"
+        Me.Text_state.Size = New System.Drawing.Size(132, 25)
+        Me.Text_state.TabIndex = 10
+        '
+        'Text_speed
+        '
+        Me.Text_speed.Location = New System.Drawing.Point(780, 111)
+        Me.Text_speed.Margin = New System.Windows.Forms.Padding(4)
+        Me.Text_speed.Name = "Text_speed"
+        Me.Text_speed.Size = New System.Drawing.Size(132, 25)
+        Me.Text_speed.TabIndex = 11
+        '
+        'Cmb_scan
+        '
+        Me.Cmb_scan.FormattingEnabled = True
+        Me.Cmb_scan.Location = New System.Drawing.Point(121, 41)
+        Me.Cmb_scan.Margin = New System.Windows.Forms.Padding(4)
+        Me.Cmb_scan.Name = "Cmb_scan"
+        Me.Cmb_scan.Size = New System.Drawing.Size(160, 23)
+        Me.Cmb_scan.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(288, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 15)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "比特率："
+        '
+        'Cmb_baud
+        '
+        Me.Cmb_baud.FormattingEnabled = True
+        Me.Cmb_baud.Items.AddRange(New Object() {"1200", "2400", "4800", "9600", "19200", "38400", "57600", "115200"})
+        Me.Cmb_baud.Location = New System.Drawing.Point(348, 39)
+        Me.Cmb_baud.Name = "Cmb_baud"
+        Me.Cmb_baud.Size = New System.Drawing.Size(121, 23)
+        Me.Cmb_baud.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1067, 562)
+        Me.Controls.Add(Me.Cmb_baud)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Cmb_scan)
+        Me.Controls.Add(Me.Text_speed)
+        Me.Controls.Add(Me.Text_state)
+        Me.Controls.Add(Me.L_speed)
+        Me.Controls.Add(Me.L_state)
+        Me.Controls.Add(Me.B_blue)
+        Me.Controls.Add(Me.B_open)
+        Me.Controls.Add(Me.B_back)
+        Me.Controls.Add(Me.B_stop)
+        Me.Controls.Add(Me.B_right)
+        Me.Controls.Add(Me.B_left)
+        Me.Controls.Add(Me.B_run)
+        Me.Controls.Add(Me.B_close)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents B_run As Button
+    Friend WithEvents B_left As Button
+    Friend WithEvents B_right As Button
+    Friend WithEvents B_stop As Button
+    Friend WithEvents B_back As Button
+    Friend WithEvents B_open As Button
+    Friend WithEvents B_close As Button
+    Friend WithEvents B_blue As Button
+    Friend WithEvents L_state As Label
+    Friend WithEvents L_speed As Label
+    Friend WithEvents Text_state As TextBox
+    Friend WithEvents Text_speed As TextBox
+    Friend WithEvents Cmb_scan As ComboBox
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Cmb_baud As ComboBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Car_Platform/car_test_form/WindowsApp1/Form1.vb
@@ -0,0 +1,115 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Text_state.Text = "stop"
+        Text_speed.Text = "100"
+
+        Cmb_baud.SelectedItem = "9600"
+
+
+
+        Me.CenterToParent()
+        B_open.Enabled = False
+        B_open.BringToFront()
+
+        B_close.Enabled = False
+        B_close.SendToBack()
+
+        B_run.Enabled = False
+        B_back.Enabled = False
+        B_stop.Enabled = False
+        B_left.Enabled = False
+        B_right.Enabled = False
+
+
+
+    End Sub
+
+    Private Sub B_run_Click(sender As Object, e As EventArgs) Handles B_run.Click
+        Text_state.Text = "run"
+        SerialPort1.Write(Text_state.Text)
+    End Sub
+
+    Private Sub B_left_Click(sender As Object, e As EventArgs) Handles B_left.Click
+        Text_state.Text = "left"
+        SerialPort1.Write(Text_state.Text)
+    End Sub
+
+    Private Sub B_stop_Click(sender As Object, e As EventArgs) Handles B_stop.Click
+        Text_state.Text = "stop"
+        SerialPort1.Write(Text_state.Text)
+    End Sub
+
+    Private Sub B_right_Click(sender As Object, e As EventArgs) Handles B_right.Click
+        Text_state.Text = "right"
+        SerialPort1.Write(Text_state.Text)
+    End Sub
+
+    Private Sub B_back_Click(sender As Object, e As EventArgs) Handles B_back.Click
+        Text_state.Text = "back"
+        SerialPort1.Write(Text_state.Text)
+    End Sub
+
+    Private Sub B_blue_Click(sender As Object, e As EventArgs) Handles B_blue.Click
+        Cmb_scan.Items.Clear() ''清除状态栏
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            Cmb_scan.Items.Add(sp)           'CompList是一个下列框控件，这里修改为你的下列框名称
+        Next
+        Cmb_scan.Text = Cmb_scan.Items(0)
+
+        B_open.Enabled = True
+        B_open.BringToFront()
+        'Cmb_scan.Items.Clear() ''清除状态栏
+        'Dim myport As Array
+        'Dim i As Integer
+        'myport = IO.Ports.SerialPort.GetPortNames() ''获取串口
+        'i = Cmb_scan.Items.Count
+        'i = i - i
+        'Try
+        '    Cmb_scan.SelectedIndex = i
+        'Catch ex As Exception
+        '    Dim result As DialogResult
+        '    result = MessageBox.Show("com port not detected", "Warning!!!", MessageBoxButtons.OK)
+        '    Cmb_scan.Text = ""
+        '    Cmb_scan.Items.Clear()
+        '    Call Form1_Load(Me, e)
+        'End Try
+
+    End Sub
+
+    Private Sub B_open_Click(sender As Object, e As EventArgs) Handles B_open.Click
+        B_open.Enabled = False
+        B_open.SendToBack()
+
+        SerialPort1.BaudRate = Cmb_baud.SelectedItem
+        SerialPort1.PortName = Cmb_scan.SelectedItem
+        SerialPort1.Open()
+
+        B_close.Enabled = True
+        B_close.BringToFront()
+
+        B_run.Enabled = True
+        B_back.Enabled = True
+        B_stop.Enabled = True
+        B_left.Enabled = True
+        B_right.Enabled = True
+
+
+    End Sub
+
+    Private Sub B_close_Click(sender As Object, e As EventArgs) Handles B_close.Click
+        B_close.Enabled = False
+        B_close.SendToBack()
+
+        SerialPort1.Close()
+        B_open.Enabled = True
+        B_open.BringToFront()
+
+        B_run.Enabled = False
+        B_back.Enabled = False
+        B_stop.Enabled = False
+        B_left.Enabled = False
+        B_right.Enabled = False
+    End Sub
+End Class
